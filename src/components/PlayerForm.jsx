02f52e751f67e7019c0454c794b031bb5e9b76dc
Caio Lima <caiojlimah@gmail.com>
2022-01-15 23:07:30 -0300

    Adicionando contador de pontos de habilidade

--- src/components/PlayerForm.jsx
+++ src/components/PlayerForm.jsx
@@ -15,17 +15,19 @@ const PlayerForm = ({ player, handleSubmit }) => {
 
   return (
     <form onSubmit={ (event) => {statsValidation(event, state); clearInput();} }>
+      <h2>Personagem{ player }:</h2>
+      <h3>Pontos restantes: { 50 - Number(state[`atk${player}`]) - Number(state[`def${player}`]) - Number(state[`hp${player}`])} </h3>
       <label htmlFor="atk">
         Atk do Personagem{ player }
-        <input onChange={ handleChange } id={`atk${player}`} type="number" value={ state[`atk${player}`] } />
+        <input onChange={ handleChange } id={`atk${player}`} type="number" min={ 0 } value={ state[`atk${player}`] } />
       </label>
       <label htmlFor="def">
         Def do Personagem{ player }
-        <input onChange={ handleChange } id={`def${player}`} type="number" value={ state[`def${player}`] } />
+        <input onChange={ handleChange } id={`def${player}`} type="number" min={ 0 } value={ state[`def${player}`] } />
       </label>
       <label htmlFor="hp">
         Hp do Personagem{ player }
-        <input onChange={ handleChange } id={`hp${player}`} type="number" value={ state[`hp${player}`] } />
+        <input onChange={ handleChange } id={`hp${player}`} type="number" min={ 0 } value={ state[`hp${player}`] } />
       </label>
       <button type="submit">Salvar</button>
     </form>
